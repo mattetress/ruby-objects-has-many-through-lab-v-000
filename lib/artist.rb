cc0c6809d2814a9f2b597ef196bc5cdb0b0f6130
Matt Etress <mattetress@me.com>
2018-10-11 02:42:28 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,23 @@
 class Artist
+  attr_reader :name
+  
+  @@all = []
 
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_song(name, genre)
+    song = Song.new(name, genre)
+    song.artist = self
+  end
+
+
+
+  
 end
