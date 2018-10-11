75160ecd10227ca8a5576fcec692c6208baa192d
Matt Etress <mattetress@me.com>
2018-10-11 03:32:05 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,3 +1,26 @@
 class Patient
+  attr_reader :name
+
+  @@all = []
+
+  def self.all 
+    @@all 
+  end
+
+  def initialize(name)
+    @name = name
+  end
+
+  def new_appointment(doctor, date)
+    appointment = Appointment.new(date, self, doctor)
+  end
+
+  def appointments
+
+  end
+
+
+
+
 
 end
